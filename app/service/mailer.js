939b3feddcd8b0aab1b09b0e74318100e7755ae3
hyj1991 <yeekwanvong@gmail.com>
2020-06-02 10:35:16 +0800

    refactor: get mail address from user

--- app/service/mailer.js
+++ app/service/mailer.js
@@ -28,7 +28,7 @@ class MailerService extends Service {
     // send email
     mailer.sendMail({
       from: auth.user,
-      to: users.map(user => user.name).join(','),
+      to: users.map(user => user.mail).join(','),
       subject: 'Easy-Monitor 性能监控',
       // text: content,
       html,
