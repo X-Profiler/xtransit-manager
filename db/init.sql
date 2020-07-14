ff0e10a03ca5aefb188ecb0491cb462257dee813
小猪 <zhuyunling@didiglobal.com>
2020-07-14 16:12:36 +0800

    add sql comment

--- db/init.sql
+++ db/init.sql
@@ -1,71 +1,71 @@
 -- template: `process-${MM-DD}`
 DROP TABLE IF EXISTS `process`;
 CREATE TABLE `process`(
-  `id` INT UNSIGNED AUTO_INCREMENT,
-  `app` INT NOT NULL,
-  `agent` VARCHAR(50) NOT NULL,
-  `pid` INT NOT NULL,
-  `uptime` INT UNSIGNED COMMENT 'prcess uptime (sec)',
+  `id` INT UNSIGNED AUTO_INCREMENT COMMENT 'unique auto increment id',
+  `app` INT NOT NULL COMMENT 'app id',
+  `agent` VARCHAR(50) NOT NULL COMMENT 'agent name',
+  `pid` INT NOT NULL COMMENT 'process id',
+  `uptime` INT UNSIGNED COMMENT 'process uptime (sec)',
   `log_time` DATETIME NOT NULL COMMENT 'process log created time on agent',
   `version` VARCHAR(50) NOT NULL COMMENT 'xprofiler version',
 
   -- cpu
-  `cpu_now` FLOAT(5,2),
-  `cpu_15` FLOAT(5,2),
-  `cpu_30` FLOAT(5,2),
-  `cpu_60` FLOAT(5,2),
+  `cpu_now` FLOAT(5,2) COMMENT 'current process cpu usage (0-100%)',
+  `cpu_15` FLOAT(5,2) COMMENT 'process cpu usage in last 15 sec (0-100%)',
+  `cpu_30` FLOAT(5,2) COMMENT 'process cpu usage in last 30 sec (0-100%)',
+  `cpu_60` FLOAT(5,2) COMMENT 'process cpu usage in last 60 sec (0-100%)',
 
   -- memory
   -- overview
-  `rss` BIGINT,
-  `heap_used` BIGINT UNSIGNED,
-  `heap_available` BIGINT UNSIGNED,
-  `heap_total` BIGINT UNSIGNED,
-  `heap_limit` BIGINT UNSIGNED,
-  `heap_executeable` BIGINT UNSIGNED,
-  `total_physical_size` BIGINT UNSIGNED,
-  `malloced_memory` BIGINT UNSIGNED,
-  `amount_of_external_allocated_memory` BIGINT UNSIGNED,
+  `rss` BIGINT COMMENT 'resident set size (byte)',
+  `heap_used` BIGINT UNSIGNED COMMENT 'heap used (byte)',
+  `heap_available` BIGINT UNSIGNED COMMENT 'heap available (byte)',
+  `heap_total` BIGINT UNSIGNED COMMENT 'heap total (byte)',
+  `heap_limit` BIGINT UNSIGNED COMMENT 'heap limit (byte)',
+  `heap_executeable` BIGINT UNSIGNED COMMENT 'heap executeable (byte)',
+  `total_physical_size` BIGINT UNSIGNED COMMENT 'heap executeable (byte)',
+  `malloced_memory` BIGINT UNSIGNED COMMENT 'malloced memory (byte)',
+  `amount_of_external_allocated_memory` BIGINT UNSIGNED COMMENT 'external allocated memory (byte)',
   -- new space size
-  `new_space_size` BIGINT UNSIGNED,
-  `new_space_used` BIGINT UNSIGNED,
-  `new_space_available` BIGINT UNSIGNED,
-  `new_space_committed` BIGINT UNSIGNED,
+  `new_space_size` BIGINT UNSIGNED COMMENT 'new space total (byte)',
+  `new_space_used` BIGINT UNSIGNED COMMENT 'new space used (byte)',
+  `new_space_available` BIGINT UNSIGNED COMMENT 'new space available (byte)',
+  `new_space_committed` BIGINT UNSIGNED COMMENT 'new space actually applies for physical memory (byte)',
   -- old space size
-  `old_space_size` BIGINT UNSIGNED,
-  `old_space_used` BIGINT UNSIGNED,
-  `old_space_available` BIGINT UNSIGNED,
-  `old_space_committed` BIGINT UNSIGNED,
+  `old_space_size` BIGINT UNSIGNED COMMENT 'old space total (byte)',
+  `old_space_used` BIGINT UNSIGNED COMMENT 'old space used (byte)',
+  `old_space_available` BIGINT UNSIGNED COMMENT 'old space available (byte)',
+  `old_space_committed` BIGINT UNSIGNED COMMENT 'old space actually applies for physical memory (byte)',
   -- code space size
-  `code_space_size` BIGINT UNSIGNED,
-  `code_space_used` BIGINT UNSIGNED,
-  `code_space_available` BIGINT UNSIGNED,
-  `code_space_committed` BIGINT UNSIGNED,
+  `code_space_size` BIGINT UNSIGNED COMMENT 'code space total (byte)',
+  `code_space_used` BIGINT UNSIGNED COMMENT 'code space used (byte)',
+  `code_space_available` BIGINT UNSIGNED COMMENT 'code space available (byte)',
+  `code_space_committed` BIGINT UNSIGNED COMMENT 'code space actually applies for physical memory (byte)',
   -- map space size
-  `map_space_size` BIGINT UNSIGNED,
-  `map_space_used` BIGINT UNSIGNED,
-  `map_space_available` BIGINT UNSIGNED,
-  `map_space_committed` BIGINT UNSIGNED,
+  `map_space_size` BIGINT UNSIGNED COMMENT 'hidden class space total (byte)',
+  `map_space_used` BIGINT UNSIGNED COMMENT 'hidden class space used (byte)',
+  `map_space_available` BIGINT UNSIGNED COMMENT 'hidden class space available (byte)',
+  `map_space_committed` BIGINT UNSIGNED COMMENT 'hidden class space actually applies for physical memory (byte)',
   -- large object space size
-  `lo_space_size` BIGINT UNSIGNED,
-  `lo_space_used` BIGINT UNSIGNED,
-  `lo_space_available` BIGINT UNSIGNED,
-  `lo_space_committed` BIGINT UNSIGNED,
+  `lo_space_size` BIGINT UNSIGNED COMMENT 'large object space total (byte)',
+  `lo_space_used` BIGINT UNSIGNED COMMENT 'large object space used (byte)',
+  `lo_space_available` BIGINT UNSIGNED COMMENT 'large object space available (byte)',
+  `lo_space_committed` BIGINT UNSIGNED COMMENT 'large object space actually applies for physical memory (byte)',
   -- read only space size
-  `read_only_space_size` BIGINT UNSIGNED,
-  `read_only_space_used` BIGINT UNSIGNED,
-  `read_only_space_available` BIGINT UNSIGNED,
-  `read_only_space_committed` BIGINT UNSIGNED,
+  `read_only_space_size` BIGINT UNSIGNED COMMENT 'read only space total (byte)',
+  `read_only_space_used` BIGINT UNSIGNED COMMENT 'read only space used (byte)',
+  `read_only_space_available` BIGINT UNSIGNED COMMENT 'read onlyread only space available (byte)',
+  `read_only_space_committed` BIGINT UNSIGNED COMMENT 'read only space actually applies for physical memory (byte)',
   -- new large object space size
-  `new_lo_space_size` BIGINT UNSIGNED,
-  `new_lo_space_used` BIGINT UNSIGNED,
-  `new_lo_space_available` BIGINT UNSIGNED,
-  `new_lo_space_committed` BIGINT UNSIGNED,
+  `new_lo_space_size` BIGINT UNSIGNED COMMENT 'new large object space total (byte)',
+  `new_lo_space_used` BIGINT UNSIGNED COMMENT 'new large object space used (byte)',
+  `new_lo_space_available` BIGINT UNSIGNED COMMENT 'new large object space available (byte)',
+  `new_lo_space_committed` BIGINT UNSIGNED COMMENT 'new large object space actually applies for physical memory (byte)',
   -- code large object space size
-  `code_lo_space_size` BIGINT UNSIGNED,
-  `code_lo_space_used` BIGINT UNSIGNED,
-  `code_lo_space_available` BIGINT UNSIGNED,
-  `code_lo_space_committed` BIGINT UNSIGNED,
+  `code_lo_space_size` BIGINT UNSIGNED COMMENT 'code large object space total (byte)',
+  `code_lo_space_used` BIGINT UNSIGNED COMMENT 'code large object space used (byte)',
+  `code_lo_space_available` BIGINT UNSIGNED COMMENT 'code large object space available (byte)',
+  `code_lo_space_committed` BIGINT UNSIGNED COMMENT 'code large object space actually applies for physical memory (byte)',
 
   -- gc
   `total_gc_times` INT UNSIGNED COMMENT 'total count of gc',
@@ -79,60 +79,60 @@ CREATE TABLE `process`(
   `incremental_marking_duration_last_record` INT UNSIGNED COMMENT 'incremental marking duration of last gc',
 
   -- uv handles
-  `active_handles` INT UNSIGNED,
-  `active_file_handles` INT UNSIGNED,
-  `active_and_ref_file_handles` INT UNSIGNED,
-  `active_tcp_handles` INT UNSIGNED,
-  `active_and_ref_tcp_handles` INT UNSIGNED,
-  `active_udp_handles` INT UNSIGNED,
-  `active_and_ref_udp_handles` INT UNSIGNED,
-  `active_timer_handles` INT UNSIGNED,
-  `active_and_ref_timer_handles`  INT UNSIGNED,
+  `active_handles` INT UNSIGNED COMMENT 'active and reference Libuv handles',
+  `active_file_handles` INT UNSIGNED COMMENT 'active file handles',
+  `active_and_ref_file_handles` INT UNSIGNED COMMENT 'active and reference file handles',
+  `active_tcp_handles` INT UNSIGNED COMMENT 'active tcp handles',
+  `active_and_ref_tcp_handles` INT UNSIGNED COMMENT 'active and reference tcp handles',
+  `active_udp_handles` INT UNSIGNED COMMENT 'active udp handles',
+  `active_and_ref_udp_handles` INT UNSIGNED COMMENT 'active and reference udp handles',
+  `active_timer_handles` INT UNSIGNED COMMENT 'active timer handles',
+  `active_and_ref_timer_handles`  INT UNSIGNED COMMENT 'active and reference timer handles',
 
   -- http
-  `response_codes` VARCHAR(1024) DEFAULT '',
-  `live_http_request` INT UNSIGNED,
-  `http_response_close` INT UNSIGNED,
-  `http_response_sent` INT UNSIGNED,
-  `http_request_timeout` INT UNSIGNED,
-  `http_patch_timeout` INT UNSIGNED,
-  `http_rt` DOUBLE,
+  `response_codes` VARCHAR(1024) DEFAULT '' COMMENT 'responese codes and count',
+  `live_http_request` INT UNSIGNED COMMENT 'live http requests in last 1 min',
+  `http_response_close` INT UNSIGNED COMMENT 'http connections close in last 1 min',
+  `http_response_sent` INT UNSIGNED COMMENT 'http responeses sent in last 1 min',
+  `http_request_timeout` INT UNSIGNED COMMENT 'timeout http requests in last 1 min',
+  `http_patch_timeout` INT UNSIGNED COMMENT 'http patch timeout (s)',
+  `http_rt` DOUBLE COMMENT 'http average response time (ms)',
 
-  `gm_modified` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `gm_create` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `gm_modified` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'system modify timestamp',
+  `gm_create` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'system create timestamp',
   PRIMARY KEY (`id`),
   INDEX (`app`, `agent`, `pid`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'process info table';
 
 -- template: `osinfo-${MM-DD}`
 DROP TABLE IF EXISTS `osinfo`;
 CREATE TABLE `osinfo`(
-  `id` INT UNSIGNED AUTO_INCREMENT,
-  `app` INT NOT NULL,
-  `agent` VARCHAR(50) NOT NULL,
+  `id` INT UNSIGNED AUTO_INCREMENT COMMENT 'unique auto increment id',
+  `app` INT NOT NULL COMMENT 'app id',
+  `agent` VARCHAR(50) NOT NULL COMMENT 'agent name',
   `uptime` INT UNSIGNED NOT NULL COMMENT 'system uptime',
   `log_time` DATETIME NOT NULL COMMENT 'system log created time on agent',
   `position` TINYINT UNSIGNED NOT NULL COMMENT '0: from system_log, 1: from xprofiler_log',
   `version` VARCHAR(50) DEFAULT '' COMMENT 'xprofiler version',
 
   -- cpu
-  `used_cpu` DOUBLE,
-  `cpu_count` INT UNSIGNED,
+  `used_cpu` DOUBLE COMMENT 'cpu usage (0-100%)',
+  `cpu_count` INT UNSIGNED COMMENT 'cpu count',
 
   -- mem
-  `total_memory` BIGINT UNSIGNED,
-  `free_memory` BIGINT UNSIGNED,
+  `total_memory` BIGINT UNSIGNED COMMENT 'os total memory (byte)',
+  `free_memory` BIGINT UNSIGNED COMMENT 'os free memory (byte)',
 
   -- load
-  `load1` DOUBLE,
-  `load5` DOUBLE,
-  `load15` DOUBLE,
+  `load1` DOUBLE COMMENT 'os load in last 1 min',
+  `load5` DOUBLE COMMENT 'os load in last 5 min',
+  `load15` DOUBLE COMMENT 'os load in last 15 min',
 
   -- disks
-  `disks` VARCHAR(1024),
+  `disks` VARCHAR(1024) COMMENT 'monitored disks directory and usage (0-100%)',
 
   -- node count
-  `node_count` INT UNSIGNED,
+  `node_count` INT UNSIGNED COMMENT 'node process count',
 
   -- gc
   `total_gc_times` INT UNSIGNED COMMENT 'total count of gc (all process)',
@@ -146,30 +146,30 @@ CREATE TABLE `osinfo`(
   `incremental_marking_duration_last_record` INT UNSIGNED COMMENT 'incremental marking duration of last gc (all process)',
 
   -- http
-  `response_codes` VARCHAR(1024) DEFAULT '',
-  `live_http_request` INT UNSIGNED,
-  `http_response_close` INT UNSIGNED,
-  `http_response_sent` INT UNSIGNED,
-  `http_request_timeout` INT UNSIGNED,
-  `http_patch_timeout` INT UNSIGNED,
-  `http_rt` DOUBLE,
+  `response_codes` VARCHAR(1024) DEFAULT '' COMMENT 'responese codes and count',
+  `live_http_request` INT UNSIGNED COMMENT 'live http requests in last 1 min',
+  `http_response_close` INT UNSIGNED COMMENT 'http connections close in last 1 min',
+  `http_response_sent` INT UNSIGNED COMMENT 'http responeses sent in last 1 min',
+  `http_request_timeout` INT UNSIGNED COMMENT 'timeout http requests in last 1 min',
+  `http_patch_timeout` INT UNSIGNED COMMENT 'http patch timeout (s)',
+  `http_rt` DOUBLE COMMENT 'http average response time (ms)',
 
-  `gm_modified` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `gm_create` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `gm_modified` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'system modify timestamp',
+  `gm_create` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'system create timestamp',
   PRIMARY KEY (`id`),
   INDEX (`app`, `agent`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'os info table';
 
 -- template: `alarm-${MM-DD}`
 DROP TABLE IF EXISTS `alarm`;
 CREATE TABLE `alarm`(
-  `id` INT UNSIGNED AUTO_INCREMENT,
-  `strategy` INT UNSIGNED NOT NULL,
-  `agent` VARCHAR(50) NOT NULL,
-  `message` VARCHAR(250) NOT NULL,
-  `pid` INT DEFAULT NULL,
-  `gm_modified` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  `gm_create` DATETIME DEFAULT CURRENT_TIMESTAMP,
+  `id` INT UNSIGNED AUTO_INCREMENT COMMENT 'unique auto increment id',
+  `strategy` INT UNSIGNED NOT NULL COMMENT 'strategy id',
+  `agent` VARCHAR(50) NOT NULL COMMENT 'agent name',
+  `message` VARCHAR(250) NOT NULL COMMENT 'alerm message',
+  `pid` INT DEFAULT NULL COMMENT 'process id',
+  `gm_modified` DATETIME DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP COMMENT 'system modify timestamp',
+  `gm_create` DATETIME DEFAULT CURRENT_TIMESTAMP COMMENT 'system create timestamp',
   PRIMARY KEY (`id`),
   INDEX (`strategy`)
-)ENGINE=InnoDB DEFAULT CHARSET=utf8;
+)ENGINE=InnoDB DEFAULT CHARSET=utf8 COMMENT 'alarm info table';
